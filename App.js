264e3427d861a1a7d74fc864fb1d53f7c32a98ce
shpatberisha <shpatberisha2510@gmail.com>
2026-05-19 16:08:34 +0200

    HG

--- App.js
+++ App.js
@@ -3,17 +3,13 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MainScreen from './screens/MainScreen';
+import ListScreen from './screens/ListScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={MainScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-
+    <ListScreen/>
   );
 }
 
@@ -21,7 +17,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   
   },
 });
