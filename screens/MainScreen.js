5d372d0f275be17a98f825bd027db03880aaddc9
shpatberisha <shpatberisha2510@gmail.com>
2026-05-21 15:59:51 +0200

    jfgt

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { Text , StyleSheet } from "react-native";
+// import React from "react";
+// import { Text , StyleSheet } from "react-native";
 
 
-const MainScreen =() => {
-    return <Text style={styles.textStyle}>Hello World</Text>;
-};
+// const MainScreen =() => {
+//     return <Text style={styles.textStyle}>Hello World</Text>;
+// };
 
-const styles = StyleSheet.create({
-    textStyle: {
-        fontSize: 30
-    }
-});
+// const styles = StyleSheet.create({
+//     textStyle: {
+//         fontSize: 30
+//     }
+// });
 
-export default MainScreen
+// export default MainScreen
